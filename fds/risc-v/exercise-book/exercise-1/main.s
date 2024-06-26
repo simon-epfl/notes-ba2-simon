c17a355f06d521b3a87d6d86ac9947fe2f910c2e
Androz 2091 <androz2091@gmail.com>
2024-06-26 13:14:33 +0200

    add logic

--- fds/risc-v/exercise-book/exercise-1/main.s
+++ fds/risc-v/exercise-book/exercise-1/main.s
@@ -1,4 +1,4 @@
-    li   x1, 0x00123456
+    li   x1, 0x8BADF00D
     li   x2, 0
     li   x3, 1
     li   x4, 0
@@ -6,6 +6,6 @@
 loop:
     and  x4, x1, x3
     add  x2, x2, x4
-    srai x1, x1, 1
+    srli x1, x1, 1
     bne  x1, x0, loop
 
